4ca140a9df2f198b26b1ea0c6b645fb63e3786dd
vargajoe <varga.yosh@gmail.com>
2022-02-26 22:12:09 +0100

    fix schema export

--- src/sn-liveei/export/LiveExport.cs
+++ src/sn-liveei/export/LiveExport.cs
@@ -26,7 +26,7 @@ namespace SnLiveExportImport
             // prepare ctd info
             ContentTypes = GetCtds();
 
-            ContentTypeContents = Content.QueryAsync("+InTree:'/Root/System/Schema' +Type:ContentType").GetAwaiter().GetResult().ToList();
+            ContentTypeContents = Content.QueryAsync("+InTree:'/Root/System/Schema' +Type:ContentType .AUTOFILTERS:OFF").GetAwaiter().GetResult().ToList();
 
             // prepare field info
             ContentFields = GetFields(ContentTypes);
